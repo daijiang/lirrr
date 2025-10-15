e25b8526cd5c405017d99e96d34c0ed71c45497c
Daijiang Li <daijianglee@gmail.com>
2025-10-15 18:04:59 -0500

    update grid cells func

--- R/spatial.R
+++ R/spatial.R
@@ -1,25 +1,33 @@
 #' Make grid cells across the lands of world 
 #' 
 #' @param cell_size The size of the cell, default is 100 km by 100 km.
+#' @param exclude_Antarctica Exclude Antarctica in the land grid cells? Default is TRUE.
 #' @param ... Additional arguments to be passed into `sf::st_make_grid()`.
 #' @return A list of two sf objectives: `grid_sf` is the grid cells of the world; 
-#' `grid_land` only contains the grid cells that intersected with terrestrial lands,
-#' which can be further filtered by continent, or country, etc.
+#' `grid_land` only contains the grid cells that intersected with terrestrial lands.
 #' @export
 #' 
-make_world_grids = function(cell_size = 100000, ...){
-  grid <- sf::st_make_grid(lirrr::world_moll, cellsize = cell_size, ...) # 100 km = 100,000 m
+make_world_grids = function(cell_size = 100000, exclude_Antarctica = TRUE, ...){
+  world_moll = lirrr::world_moll
+  grid <- sf::st_make_grid(world_moll, cellsize = cell_size) # 100 km = 100,000 m
   grid_sf <- sf::st_sf(
     cell_id = paste0("cell_", 1:length(grid)),
     geometry = grid
   )
   
   # keep only grid cells that intersect land
-  grid_land <- sf::st_intersection(grid_sf, world_moll)
+  if(exclude_Antarctica){
+    world_moll = dplyr::filter(world_moll, continent != "Antarctica")
+  }
+  idx <- sf::st_intersects(grid_sf, world_moll, sparse = TRUE)
+  has_land <- lengths(idx) > 0
+  grid_land <- grid_sf[has_land, ]
+  grid_land = unique(grid_land)
+  
   # grid_land <- sf::st_intersection(grid_sf, dplyr::filter(world_moll, ! continent %in% c("Antarctica")))
   # dplyr::count(st_drop_geometry(grid_land), continent)
   
-  list(grid_sf, grid_land)
+  list(grids_world = grid_sf, grids_land = grid_land)
 }
 
 
--- data-raw/world_spatial.R
+++ data-raw/world_spatial.R
@@ -10,9 +10,11 @@ crs_equal_area <- "ESRI:54009"
 
 # 3. Reproject world to equal-area
 world_moll <- st_transform(world, crs_equal_area)
+world_moll <- st_simplify(lirrr::world_moll, dTolerance = 1000)  # meters
 
 # 4. Check bbox
 st_bbox(world_moll)
 plot(st_geometry(world_moll))
 
-usethis::use_data(world_moll)
+
+usethis::use_data(world_moll, overwrite = TRUE)
